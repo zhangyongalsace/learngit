4cd813a331551da9a0db98a35d28b019217c8c60
zhangyongalsace <yong.z@unisoc.com>
2018-11-14 14:31:51 +0800

    das

--- zhangyongtest.c
+++ zhangyongtest.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 int main(){
-dfasfsda 
-dasfasdf dsadfa s
 	printf("nimei de \n");
 	printf("hello  world!\n");
-	fdsafea fdsafsda
 	printf("hali!\n");
 	return 0;
 }
